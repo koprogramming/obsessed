8b47f24a5e7f02dac8d89ca6dd12e898269de4bf
k0p <khaliliorientedprogramming@gmail.com>
2022-12-03 17:56:11 -0800

    Add a wikilink parser - now we can clickety clack the links!!!

--- main.py
+++ main.py
@@ -1,21 +1,54 @@
 import glob
 import os
+import shutil
 import sys
 
 import markdown
+from markdown.extensions.wikilinks import WikiLinkExtension
+
+from CoolWikilinksExtension import CoolWikilinkExtension
+from utils import modify_name
 
 OUTPUT_FOLDER = "build"
 
+HEADER = ""
+FOOTER = ""
 
-def get_markdown_files(path):
+
+with open("fragments/_foot.html") as f, open("fragments/_head.html") as h:
+    HEADER = h.read()
+    FOOTER = f.read()
+
+
+def add_header_and_footer(html_text):
+    """
+    Adds a header and a footer
+    """
+    return HEADER + html_text + FOOTER
+
+
+def delete_build_folder(folder):
+    shutil.rmtree("/folder_name", ignore_errors=True)
+
+
+def create_site(path):
+
+    delete_build_folder(f"{path}/{OUTPUT_FOLDER}")
 
     for notes_file in glob.glob("*.md", root_dir=path):
         with open(f"{path}/{notes_file}") as f:
             content = f.read()
-            html = markdown.markdown(content)
+            html = markdown.markdown(content, extensions=[CoolWikilinkExtension()])
+            finished = add_header_and_footer(html)
+            notes_file_name = modify_name(notes_file)
+            folder_to_make = f"{path}/{OUTPUT_FOLDER}/{notes_file_name}"
+            if not notes_file == "index.md":
+                os.makedirs(folder_to_make)
 
-            with open(f"{path}/{OUTPUT_FOLDER}/{notes_file[:-3]}.html", "w") as h:
-                h.write(html)
+            if notes_file == "index.md":
+                folder_to_make = f"{path}/{OUTPUT_FOLDER}"
+            with open(f"{folder_to_make}/index.html", "w") as h:
+                h.write(finished)
 
 
 if __name__ == "__main__":
@@ -27,6 +60,6 @@ if __name__ == "__main__":
         print("you need to give a directory!")
         exit
 
-    get_markdown_files(path)
+    create_site(path)
 
     print("... done!")
